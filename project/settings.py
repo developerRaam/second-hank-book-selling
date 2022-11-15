f72101b7a2df9408172e4d6d1cf858617c2f9268
ramnarayan maurya <rnm312000@gmail.com>
2022-11-16 01:54:09 +0530

    version 1

--- project/settings.py
+++ project/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
